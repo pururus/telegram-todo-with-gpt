2c093f5b34bb2f3833df6a1ff274a245b82eb9fd
Святослав Полонский <svatoslavpolonskiy@192.168.1.10>
2024-11-19 17:03:39 +0300

    some dataclasses

---
+++ Project/Query.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Query:
+    """
+        Client's query, forwarded from telebot to GPT_module
+        
+        Attributes
+        __________
+        client_id: str - telegram id in @<id> format
+        
+        current_time: datetime - current time and date
+        
+        content: str - query body
+    """
+    client_id: str
+    current_time: datetime
+    content: str
+
+@dataclass
+class RegistrationQuery:
+    """
+        Client's query for registration
+        
+        Attributes
+        __________
+        client_id: str - telegram id in @<id> format
+        
+        Calendar_link: str
+        
+        Notion_link: str
+    """
+    client_id: str
+    Calendar_link: str
+    Notion_link: str
