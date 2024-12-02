9e0d6dd321bcffc670d2af25d9b1fe9ebfa909bb
Святослав Полонский <svatoslavpolonskiy@192.168.1.13>
2024-12-02 15:21:33 +0300

    calendar gets Request

--- Project/Calendar/Calendar_module.py
+++ Project/Calendar/Calendar_module.py
@@ -2,8 +2,10 @@ from __future__ import print_function
 import googleapiclient
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from Request import Request
 
 from pathlib import Path
+from typing import Dict
 
 search_directory = Path('../')
 
@@ -26,7 +28,7 @@ class CalendarModule:
         credentials = service_account.Credentials.from_service_account_file(self.SERVICE_ACCOUNT_FILE, scopes=self.SCOPES)
         self.service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
 
-    def create_event(self, event, calendarId):
+    def create_event(self, event: Dict[str, str] | Request, calendarId):
         '''
         Creates event in calendar
         
@@ -36,6 +38,19 @@ class CalendarModule:
         
         calendarId: str - the Id of calendar where inserting
         '''
+        if type(event) == Request:
+            new_dict = Dict()
+            new_dict['summary'] = event.body
+            if event.extra:
+                new_dict["description"] = event.extra
+            new_dict["start"] = event.timefrom
+            if event.dateto:
+                new_dict["end"] = event.dateto
+            else:
+                new_dict["end"] = event.timefrom
+            
+            event = new_dict
+            
         try:
             event = self.service.events().insert(calendarId=calendarId, body=event).execute()
             print('Event created: %s' % (event.get('id')))
